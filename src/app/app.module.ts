5e99024eed2d464944e2491db7de2e9d74a1cf17
watcharin <twatcharin90@gmail.com>
2021-03-05 14:47:25 +0700

    update component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TopBannerComponent } from './top-banner/top-banner.component';
+import { CategoryMenuComponent } from './category-menu/category-menu.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    TopBannerComponent,
+    CategoryMenuComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule
